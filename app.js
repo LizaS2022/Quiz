1eb8e698e13a17c4106768d885f8a24e47c6d808
Elizabeta Sirota <elizabetasirota@gmail.com>
2023-04-10 09:47:55 -0400

    Review code

--- app.js
+++ app.js
@@ -1,31 +1,65 @@
 //once the start button is clicked, the header is empty and only the first question with the options appear on the screen//
 
 var startButtonEl = document.getElementById("start-button");
-var containerEl = document.getElementById("container1");
+var containerEl = document.getElementById("container-page1");
 var titleEl = document.getElementById("header-title");
 var contentEl = document.getElementById("start-content");
 var qaEl = document.getElementById("question-answer-container");
+let questionEl = document.getElementById("question");
 var timerContainerEl = document.getElementById("timer-container");
 var formEl = document.getElementById("input-container");
 var timeEl = document.querySelector(".time");
+var commentResultEl = document.getElementById("result-comment");
 var resultPEl = document.getElementById("text-result");
 
 var initialsEl = document.getElementById("msg");
 var submitEl = document.getElementById("submit");
 
 var tableEl = document.getElementById("highscores-container");
+var index = 0;
+var secondsLeft = 60;
+
+
+function timer(){
+    var timeIntervael = setInterval(function() {
+    secondsLeft--;
+    if (index < questionArray.length){
+        timeEl.textContent = secondsLeft;
+        console.log("seconds left: "+ secondsLeft);
+    
+    if (secondsLeft <= 0) {
+        clearInterval(timeIntervael);
+        timeEl.textContent = 0;
+        }
+    }
+},1000);
+}
+
 
 startButtonEl.addEventListener("click", function(){
+
     qaGenerator();
-    initTime();
+    for (var i = 0; i < 4; i++) {
+    let answerEl = document.getElementById("option" + (i+1));
+    answerEl.addEventListener("click", function(ev){
+    checkAnswer(ev);
+    index++;
+    if (index === questionArray.length){
+        commentResultEl.setAttribute("style", "display:none;");
+        formEl.setAttribute("style", "display: block;");
+        qaEl.setAttribute("style", "display: none;");
+        finalScore();
+        }
+    else {
+        qaGenerator();}
+     });
+    }
+    
+    timer();
     qaEl.setAttribute("style", "display:block");
     containerEl.setAttribute("style", "display:none");
-    
 });
 // questions section//
-
-
-
 // Building an array of objects wjere each object represents a question
 // with 4 possible answers button and an answer.
 
@@ -62,88 +96,52 @@ var questionArray = [
     }
 ]
 
-var index=0;
-
 
 function qaGenerator() {
-    let questionEl = document.getElementById("question");
+    console.log("Generating" + index);
     if (index < questionArray.length){
         questionEl.textContent = questionArray[index].question;
        console.log(questionArray[index].question);
         for (let i = 0; i < 4; i++) {
             let answerEl = document.getElementById("option" + (i+1));
             answerEl.textContent = questionArray[index].choices[i];
-            answerEl.addEventListener("click", checkAnswer);
+            console.log(answerEl);
         }
     }
 }
 
 
 function checkAnswer(ev){
-    var commentResultEl = document.getElementById("result-comment");
-    console.log(ev);
-
+    
     if (index >= questionArray.length) {
         return;
     }
 
     if (ev !== null) {
-        console.log(ev.target.innerText);
-        console.log(questionArray[index].answer);
+        console.log("the event target inner text is: " + ev.target.innerText);
+        console.log("the options are : " + questionArray[index].answer);
         if (ev.target.innerText === questionArray[index].answer) {
-            console.log(commentResultEl);
             commentResultEl.innerText = "Correct answer"; 
-            
+            console.log(commentResultEl);
+            console.log(index);
         }
-
         else {
             commentResultEl.innerText = "Wrong answer";
+            console.log("the answer is: " + commentResultEl);
             secondsLeft-=15;
         }
-        index++;
-        
-
-        if (index >= questionArray.length) {
-            commentResultEl.innerText = " ";
-            formEl.setAttribute("style", "display: block;");
-            qaEl.textContent = "";
-            var finalScore = timeEl.textContent
-            console.log("your final score is: "+ finalScore);
-            timeEl.textContent ="";
-            resultPEl.textContent = " your score is " + finalScore;
-            highScores(finalScore);
-            
-        }
     }
-    qaGenerator();
 }
 
-// time setter section
-
-var secondsLeft = 60;
 
 
-function initTime(){
-
-var timeIntervael = setInterval(function() {
-
-    secondsLeft--;
-
-    if (index < questionArray.length){
-    timeEl.textContent = secondsLeft;
-    if (secondsLeft <= 0) {
-        clearInterval(timeIntervael);
-        timeEl.textContent = 0;
-        }
-    }
-
-    else {
-    clearInterval(timeIntervael);
-    console.log(timeEl);
-    }
-},1000);
+function finalScore() {
+    var finalScore = timeEl.textContent;
+    console.log("your final score is: "+ finalScore);
+    timeEl.setAttribute("style", "display:none");
+    resultPEl.textContent = " your score is " + finalScore;
+    highScores(finalScore);
 }
-
 
 
 // when the user clicks submit, the input will be saved in the local storage 
@@ -152,11 +150,9 @@ function highScores(score) {
 
             submitEl.addEventListener("click", function(){
                 event.preventDefault();
-               
                 var initial = initialsEl.value.trim();
-               
+                console.log(initial);
                localStorage.setItem(initial, score);
-
                showtableScores(initial);
                });
 }
@@ -166,21 +162,36 @@ function showtableScores(initial){
     var orderedListScores = document.getElementById("table-scores");
     formEl.setAttribute("style", "display: none;");
     tableEl.setAttribute("style", "display:block;");
+    console.log(localStorage.getItem(initial));
     var getScore = JSON.parse(localStorage.getItem(initial));
     console.log(getScore);
 
-    
     var scoreItem = document.createElement("li");
+    console.log(scoreItem);
     
-
     scoreItem.textContent = getScore.toString();
     orderedListScores.appendChild(scoreItem);
-
-        
-    // });
-
+    console.log(orderedListScores);
 }
 
 
+// redo quiz
+
+var redoQuiz = document.getElementById("redo-quiz");
+console.log("the redo button shows: " +redoQuiz);
+redoQuiz.addEventListener("click", function(){
+    index = 0;
+    secondsLeft = 60;
+   
+    tableEl.textContent="";
+    qaEl.setAttribute("style", "display:block;");
+    qaGenerator();
+    timer();
+});
 
 
+
+
+    
+
+    
